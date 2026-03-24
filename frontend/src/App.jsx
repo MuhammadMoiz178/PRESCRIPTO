1d2f351386b695b65377085c6dd605e67f919627
Muhammad Moiz <moizmuhammad467@gmail.com>
2026-03-24 17:28:41 +0500

    Add About page with detailed information and user engagement features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import About from './pages/About'
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
       <Route path='/doctors/:speciality' element={<Doctors/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/contact' element={<Contact/>}/>
+      <Route path='/about' element={<About/>}/>
       <Route path='/my-profile' element={<MyProfile/>}/>
       <Route path='/my-appointments' element={<MyAppointments/>}/>
       <Route path='/appointment/:docId' element={<Appointment/>}/>
