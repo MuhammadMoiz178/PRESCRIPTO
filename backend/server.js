c40d403522c47f84d0eb937d4fe3162dbbe79278
Muhammad Moiz <moizmuhammad467@gmail.com>
2026-03-26 15:10:29 +0500

    feat: add MongoDB connection setup to backend

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-
+import connectDb from './config/mongodb.js'
 
 //app config
 const app = express()
 const PORT = process.env.PORT || 4000;
-
+connectDb()
 // middlewares
 app.use(express.json())
 app.use(cors())
