481606ee4fff38d58b119756c9ee8f620b6ec3d7
Muhammad Moiz <moizmuhammad467@gmail.com>
2026-03-28 23:38:11 +0500

    feat: implement addDoctor API with validation and image upload

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDb from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/admin.route.js'
 
 //app config
 const app = express()
@@ -14,6 +15,7 @@ app.use(express.json())
 app.use(cors())
 
 // api endpoints
+app.use('/api/admin',adminRouter)
 
 app.get('/',(req,res)=>{
     res.send('Api is working')
