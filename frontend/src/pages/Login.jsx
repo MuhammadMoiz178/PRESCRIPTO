48a41c671d930787d7ff8788944e7b421306a99a
Muhammad Moiz <moizmuhammad467@gmail.com>
2026-04-05 22:05:14 +0500

    feat: implement user login functionality with JWT token handling and integrate with registration process

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,12 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const {backendUrl,token,setToken} = useContext(AppContext)
+  const navigate = useNavigate()
   const [state,setState] = useState('Sign Up')
   const [email,setEmail] = useState('')
   const [password,setPassword] = useState('')
@@ -8,8 +14,34 @@ const Login = () => {
 
   const onSubmitHandler = async (e) => {
       e.preventDefault();
+      try {
+        if(state === 'Sign Up') {
+          const {data}= await axios.post(backendUrl + '/api/user/register',{name,password,email})
+          if(data.success) {
+            localStorage.setItem('token',data.token)
+            setToken(data.token)
+          } else {
+            toast.error(data.message)
+          }
 
+        } else {
+          const {data}= await axios.post(backendUrl + '/api/user/login',{password,email})
+          if(data.success) {
+            localStorage.setItem('token',data.token)
+            setToken(data.token)
+          } else {
+            toast.error(data.message)
+          }
+        }
+      } catch (error) {
+        toast.error(error.message)
+      }
   }
+   useEffect(()=>{
+      if(token) {
+        navigate('/')
+      }
+   },[token])
   return (
     <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
         <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border border-gray-100 rounded-xl text-zinc-600 text-sm  shadow-lg'>
@@ -37,7 +69,7 @@ const Login = () => {
             type="password" onChange={(e)=>setPassword(e.target.value)} value={password} required/>
           </div>
 
-          <button className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? "Create Account" : "Log In"}</button>
+          <button type='submit' className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? "Create Account" : "Log In"}</button>
           {
             state === 'Sign Up'
             ? <p>Already have an account? <span onClick={()=>setState('Login')} className='text-primary underline cursor-pointer'>Login here</span></p> 
